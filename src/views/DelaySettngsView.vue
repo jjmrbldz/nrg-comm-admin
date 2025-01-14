aace350e97bbfddc6eb838861b49a97b174c7bb0
jjmrbldz <jejo.baldoza@gmail.com>
2025-01-14 17:50:37 +0800

    add weekly on settings parm

--- src/views/DelaySettngsView.vue
+++ src/views/DelaySettngsView.vue
@@ -1,33 +1,42 @@
 <template>
-    <div class="formgrid grid">
-        <div class="field col">
-            <label>{{ $store.getters['languageStore/translate']('Comment Delay') }}</label>
-            <InputNumber type="name" v-model="params.tc_comment_delay" :use-grouping="true" :min-fraction-digits="0" :max-fraction-digits="0" :min="0" :max="100" class="w-full" :disabled="loading" />
-        </div>  
-        <div class="field col">
-            <label>{{ $store.getters['languageStore/translate']('Post Delay') }}</label>
-            <InputNumber type="name" v-model="params.tc_post_delay" :use-grouping="true" :min-fraction-digits="0" :max-fraction-digits="0" :min="0" :max="100" class="w-full" :disabled="loading" />
-        </div>
-        <Divider>Attendance</Divider>   
-        <div class="field col">
-            <label>{{ $store.getters['languageStore/translate']('Reward Daily') }}</label>
-            <InputNumber type="name" v-model="params.tc_attendance_reward_daily" :use-grouping="true" :min-fraction-digits="0" :max-fraction-digits="0" :min="0" class="w-full" :disabled="loading" />
-        </div>   
-        <div class="field col">
-            <label>{{ $store.getters['languageStore/translate']('Reward Monthly') }}</label>
-            <InputNumber type="name" v-model="params.tc_attendance_reward_monthly" :use-grouping="true" :min-fraction-digits="0" :max-fraction-digits="0" :min="0" class="w-full" :disabled="loading" />
-        </div>   
-        <div class="field col">
-            <label>{{ $store.getters['languageStore/translate']('Attendance On/Off') }}</label>
-            <div class="w-full pt-1">
-                <InputSwitch  v-model="params.tc_attendance_on_off" :true-value="1" :false-value="0" />
+    <Card>
+        <template #content>
+            <div class="grid">
+                <div class="col-4">
+                    <div class="field">
+                        <label>{{ $store.getters['languageStore/translate']('Comment Delay') }}</label>
+                        <InputNumber type="name" v-model="params.tc_comment_delay" :use-grouping="true" :min-fraction-digits="0" :max-fraction-digits="0" :min="0" :max="100" class="w-full" :disabled="loading" />
+                    </div>
+                    <div class="field col">
+                        <label>{{ $store.getters['languageStore/translate']('Post Delay') }}</label>
+                        <InputNumber type="name" v-model="params.tc_post_delay" :use-grouping="true" :min-fraction-digits="0" :max-fraction-digits="0" :min="0" :max="100" class="w-full" :disabled="loading" />
+                    </div>
+                    <Divider>Attendance</Divider>   
+                    <div class="field col">
+                        <label>{{ $store.getters['languageStore/translate']('Reward Daily') }}</label>
+                        <InputNumber type="name" v-model="params.tc_attendance_reward_daily" :use-grouping="true" :min-fraction-digits="0" :max-fraction-digits="0" :min="0" class="w-full" :disabled="loading" />
+                    </div>   
+                    <div class="field col">
+                        <label>{{ $store.getters['languageStore/translate']('Reward Weekly') }}</label>
+                        <InputNumber type="name" v-model="params.tc_attendance_reward_weekly" :use-grouping="true" :min-fraction-digits="0" :max-fraction-digits="0" :min="0" class="w-full" :disabled="loading" />
+                    </div>   
+                    <div class="field col">
+                        <label>{{ $store.getters['languageStore/translate']('Reward Monthly') }}</label>
+                        <InputNumber type="name" v-model="params.tc_attendance_reward_monthly" :use-grouping="true" :min-fraction-digits="0" :max-fraction-digits="0" :min="0" class="w-full" :disabled="loading" />
+                    </div>   
+                    <div class="field col">
+                        <label>{{ $store.getters['languageStore/translate']('Attendance On/Off') }}</label>
+                        <div class="w-full pt-1">
+                            <InputSwitch  v-model="params.tc_attendance_on_off" :true-value="1" :false-value="0" />
+                        </div>
+                    </div> 
+                    <div class="flex">
+                        <Button class="w-full" :label="$store.getters['languageStore/translate']('submitLang')" @click="submit" :loading="loading" />
+                    </div>
+                </div>  
             </div>
-        </div>   
-        <div class="field col">
-            <label>&nbsp;</label>
-            <Button class="w-full" :label="$store.getters['languageStore/translate']('submitLang')" @click="submit" :loading="loading" />
-        </div>
-    </div>
+        </template>
+    </Card>
 </template>
 
 <script>
@@ -42,6 +51,7 @@ export default {
                 tc_comment_delay    : null,
                 tc_post_delay       : null,
                 tc_attendance_reward_daily      : null,
+                tc_attendance_reward_weekly    : null,
                 tc_attendance_reward_monthly    : null,
                 tc_attendance_on_off            : null,
             },
@@ -60,6 +70,7 @@ export default {
                         tc_comment_delay    : this.params.tc_comment_delay,
                         tc_post_delay       : this.params.tc_post_delay,
                         tc_attendance_reward_daily      : this.params.tc_attendance_reward_daily,
+                        tc_attendance_reward_weekly    : this.params.tc_attendance_reward_weekly,
                         tc_attendance_reward_monthly    : this.params.tc_attendance_reward_monthly,
                         tc_attendance_on_off            : this.params.tc_attendance_on_off,
                     }
@@ -101,6 +112,7 @@ export default {
                         tc_comment_delay    : null,
                         tc_post_delay       : null,
                         tc_attendance_reward_daily      : null,
+                        tc_attendance_reward_weekly    : null,
                         tc_attendance_reward_monthly    : null,
                         tc_attendance_on_off            : null,
                     }
@@ -112,9 +124,10 @@ export default {
                     id                  : null,
                     tc_comment_delay    : null,
                     tc_post_delay       : null,
-                    tc_attendance_reward_daily          : null,
-                        tc_attendance_reward_monthly    : null,
-                        tc_attendance_on_off            : null,
+                    tc_attendance_reward_daily      : null,
+                    tc_attendance_reward_weekly     : null,
+                    tc_attendance_reward_monthly    : null,
+                    tc_attendance_on_off            : null,
                 }
                 throw error
             } finally {
